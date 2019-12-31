05267f597860b6ee333741368a5b1386fcc54a2a
v4g4x <varun.gawande@gmail.com>
2019-12-31 10:02:10 +0530

    Hashed it and removed Segmentation fault

--- Assembler/Pass1.cpp
+++ Assembler/Pass1.cpp
@@ -4,38 +4,61 @@
 #include<cstring>
 using namespace std;
 
-char OPTAB[13][5][6]= {	
-{"STOP","00","IS","2","0"}, 
-{"ADD","01","IS","2","2"},
-{"SUB","02","IS","2","2"},
-{"MULT","03","IS","2","2"},
-{"MOVER","04","IS","2","2"},
-{"MOVEM","05","IS","2","2"},
-{"COMP","06","IS","2","2"},
-{"BC","07","IS","2","1"},
-{"DIV","08","IS","2","2"},
-{"READ","09","IS","2","1"},
-{"PRINT","10","IS","2","1"},
-{"DC","01","DL","1","1"},
-{"DS","02","DL","SIZE","1"}
+char OPTAB[13][6][6]= {	
+/*0*/		{"MULT"	,"03","IS","2"	,"2",	"-1"},
+/*1*/		{"READ"	,"09","IS","2"	,"1",	"-1"},
+/*2*/		{"MOVEM"	,"05","IS","2"	,"2",	"3"},
+/*3*/		{"SUB"	,"02","IS","2"	,"2",	"-1"},
+/*4*/		{"BC"		,"07","IS","2"	,"1",	"5"},
+/*5*/		{"STOP"	,"00","IS","2"	,"0",	"-1"}, 
+/*6*/		{"DC"		,"01","DL","1"	,"1",	"-1"},
+/*7*/		{"COMP"	,"06","IS","2"	,"2",	"10"},
+/*8*/		{"ADD"	,"01","IS","2"	,"2",	"12"},
+/*9*/		{"DS"		,"02","DL","SIZE"	,"1",	"-1"},
+/*10*/	{"MOVER"	,"04","IS","2"	,"2",	"-1"},
+/*11*/	{"PRINT"	,"10","IS","2"	,"1",	"-1"},
+/*12*/	{"DIV"	,"08","IS","2"	,"2",	"-1"}
 };
 //OPTAB[][x] where x=0 for mnem.code , 1 for machine, 2 for class, 3 for length of IS, 4 for no. of operands
+
+int getHash(string stri){
+	char str[stri.length()+1];
+	strcpy(str,stri.c_str());
+	int val = 0;
+	for(int i=0 ; i<stri.length() ; i++)
+		val+=(str[i]-64);
+	val%=13;
+	val--;	
+	while(strcasecmp(OPTAB[val][0],str)!=0){
+		sscanf(OPTAB[val][5],"%d",&val);
+		if(val==-1)
+			break;
+	}
+	return val;
+}
+
 
 char ADTAB[5][2][7] = {
 	//ADTAB[][0] = mnem code
 	//ADTAB[][1] = intermediate machine code
-	{"START","01"},
-	{"END","02"},
-	{"LTORG","03"},	
-	{"ORIGIN","04"},	
-	{"EQU","05"},	
+	{"START"	,"01"},
+	{"END"	,"02"},
+	{"LTORG"	,"03"},	
+	{"ORIGIN"	,"04"},	
+	{"EQU"	,"05"}	
 };
 
-void Compilation_Error(string str){
+void compError(string str){
 	cout<<"Compilation Error: "<<str<<endl;
 }
 
 int main(int argc,char** argv){
+	cout<<"Hash of ADD is :"<<getHash("ADD")<<endl;
+	cout<<"Hash of COMP is :"<<getHash("COMP")<<endl;
+	cout<<"Hash of PRINT is :"<<getHash("PRINT")<<endl;
+	cout<<"Hash of STOP is :"<<getHash("STOP")<<endl;
+	cout<<"Hash of MOVER is :"<<getHash("MOVER")<<endl;
+	cout<<"Hash of HAHA is :"<<getHash("HAHA")<<endl;
 	ifstream reader;
 	reader.open(argv[1],ios::in);
 	string line;
@@ -43,30 +66,27 @@ int main(int argc,char** argv){
 	getline(reader,line);
 	strcpy(c_line,line.c_str());
 	if(strcmp(c_line,"START")!=0){
-		Compilation_Error("No \"START\" in Program.");
+		compError("No \"START\" in Program.");
 		return 1;
 	}
-	cout<<"=======================================1"<<endl;
 	while(!reader.eof()){
 		getline(reader,line);
+		if(reader.eof())
+			break;
 		char c_line[line.length()+1];
 		strcpy(c_line,line.c_str());
 		string line_tok[4];
 		char *temp = strtok(c_line," ,\t");
 		line_tok[0] = temp;
-		cout<<"=======================================2"<<endl;
 		short i=0;
 		while(temp!=NULL){
 			cout<<line_tok[i]<<"\t";
 			temp=strtok(NULL," ,\t");
-			cout<<"======================================"<<(i+3)<<endl;
+			if(temp==NULL)
+				break;
 			line_tok[++i] = temp;
 		}
-		cout<<"======================================"<<endl;
-		if(reader.eof())
-			break;
 		cout<<endl;
-//		cout<<line2<<endl;
 	}
 	reader.close();
 	return 0;
