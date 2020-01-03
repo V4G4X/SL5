2987054d1b15c94f95ca454629f558682e00560c
Shivani0608 <varun.gawande@gmail.com>
2020-01-03 09:57:49 +0530

    Pass1 Tokenization Almost done

--- Assembler/Pass1.cpp
+++ Assembler/Pass1.cpp
@@ -4,22 +4,44 @@
 #include<cstring>
 using namespace std;
 
-char OPTAB[13][6][6]= {	
-/*0*/		{"MULT"	,"03","IS","2"	,"2",	"-1"},
-/*1*/		{"READ"	,"09","IS","2"	,"1",	"-1"},
-/*2*/		{"MOVEM"	,"05","IS","2"	,"2",	"3"},
-/*3*/		{"SUB"	,"02","IS","2"	,"2",	"-1"},
-/*4*/		{"BC"		,"07","IS","2"	,"1",	"5"},
-/*5*/		{"STOP"	,"00","IS","2"	,"0",	"-1"}, 
-/*6*/		{"DC"		,"01","DL","1"	,"1",	"-1"},
-/*7*/		{"COMP"	,"06","IS","2"	,"2",	"10"},
-/*8*/		{"ADD"	,"01","IS","2"	,"2",	"12"},
-/*9*/		{"DS"		,"02","DL","SIZE"	,"1",	"-1"},
-/*10*/	{"MOVER"	,"04","IS","2"	,"2",	"-1"},
-/*11*/	{"PRINT"	,"10","IS","2"	,"1",	"-1"},
-/*12*/	{"DIV"	,"08","IS","2"	,"2",	"-1"}
+int LC=0;	//Location Counter
+int PTP=0;	//Pooltab Pointer
+int LTP=0;	//Location Table Pointer
+int STP=0;	//Symbol Table Pointer
+int POOLTAB=0;
+
+char REG[4][2][5] = {
+	//REG[x][] is used for iteration,
+	//REG[][0] ==> Register
+	//REG[][1] ==> Code
+	{"AREG","R01"},
+	{"BREG","R02"},
+	{"CREG","R03"},
+	{"DREG","R04"}
 };
-//OPTAB[][x] where x=0 for mnem.code , 1 for machine, 2 for class, 3 for length of IS, 4 for no. of operands
+char* getReg(string str){
+	for(int i=0 ; i<4 ; i++)
+		if(strcasecmp(REG[i][0],str.c_str())==0)//Register Code Match
+			return REG[i][1];
+	return NULL;
+}
+
+char MOT[13][6][6]= {	
+/*0*/		{"MULT"	,"03","IS","2"		,"2",	"-1"},
+/*1*/		{"READ"	,"09","IS","2"		,"1",	"-1"},
+/*2*/		{"MOVEM","05","IS","2"		,"2",	"3"},
+/*3*/		{"SUB"	,"02","IS","2"		,"2",	"-1"},
+/*4*/		{"BC"	,"07","IS","2"		,"1",	"5"},
+/*5*/		{"STOP"	,"00","IS","2"		,"0",	"-1"}, 
+/*6*/		{"DC"	,"01","DL","1"		,"1",	"-1"},
+/*7*/		{"COMP"	,"06","IS","2"		,"2",	"10"},
+/*8*/		{"ADD"	,"01","IS","2"		,"2",	"12"},
+/*9*/		{"DS"	,"02","DL","SIZE"	,"1",	"-1"},
+/*10*/		{"MOVER","04","IS","2"		,"2",	"-1"},
+/*11*/		{"PRINT","10","IS","2"		,"1",	"-1"},
+/*12*/		{"DIV"	,"08","IS","2"		,"2",	"-1"}
+};
+//MOT[][x] where x=0 for mnem.code , 1 for machine, 2 for class, 3 for length of IS, 4 for no. of operands
 
 int getHash(string stri){
 	char str[stri.length()+1];
@@ -29,8 +51,8 @@ int getHash(string stri){
 		val+=(str[i]-64);
 	val%=13;
 	val--;	
-	while(strcasecmp(OPTAB[val][0],str)!=0){
-		sscanf(OPTAB[val][5],"%d",&val);
+	while(strcasecmp(MOT[val][0],str)!=0){
+		sscanf(MOT[val][5],"%d",&val);
 		if(val==-1)
 			break;
 	}
@@ -38,14 +60,14 @@ int getHash(string stri){
 }
 
 
-char ADTAB[5][2][7] = {
-	//ADTAB[][0] = mnem code
-	//ADTAB[][1] = intermediate machine code
+char POT[5][2][7] = {
+	//POT[][0] = Pseudo mnem code
+	//POT[][1] = intermediate machine code
 	{"START"	,"01"},
-	{"END"	,"02"},
+	{"END"		,"02"},
 	{"LTORG"	,"03"},	
 	{"ORIGIN"	,"04"},	
-	{"EQU"	,"05"}	
+	{"EQU"		,"05"}	
 };
 
 void compError(string str){
@@ -53,40 +75,52 @@ void compError(string str){
 }
 
 int main(int argc,char** argv){
-	cout<<"Hash of ADD is :"<<getHash("ADD")<<endl;
-	cout<<"Hash of COMP is :"<<getHash("COMP")<<endl;
-	cout<<"Hash of PRINT is :"<<getHash("PRINT")<<endl;
-	cout<<"Hash of STOP is :"<<getHash("STOP")<<endl;
-	cout<<"Hash of MOVER is :"<<getHash("MOVER")<<endl;
-	cout<<"Hash of HAHA is :"<<getHash("HAHA")<<endl;
 	ifstream reader;
-	reader.open(argv[1],ios::in);
+	reader.open(argv[1],ios::in);							//Open program for input
 	string line;
 	char c_line[line.length()+1];
-	getline(reader,line);
+	getline(reader,line);								//Read first line from program
 	strcpy(c_line,line.c_str());
-	if(strcmp(c_line,"START")!=0){
-		compError("No \"START\" in Program.");
+	char* start_tok = strtok(c_line," ,\t");
+	if(strcasecmp(start_tok,"START")!=0){		
+		compError("InCorrect \"START\" in Program.");				//If first word not START, exit
 		return 1;
 	}
-	while(!reader.eof()){
-		getline(reader,line);
-		if(reader.eof())
+	int len=0;
+	while(start_tok!=NULL){
+		start_tok = strtok(NULL," ,\t")	;
+		if(start_tok==NULL && len!=0){
+			break;
+		}
+		if(++len==1){
+			sscanf(start_tok,"%d",&LC);
+			cout<<"LC Updated with "<<LC<<endl;
+		}
+		else{
+			compError("InCorrect \"START\" in Program.");			//If START incorrect
+			return 1;
+		}
+	}
+	while(!reader.eof()){								//Go till End of File
+		getline(reader,line);							//Read line
+		if(reader.eof())							//If file ended leave loop
 			break;
 		char c_line[line.length()+1];
 		strcpy(c_line,line.c_str());
-		string line_tok[4];
-		char *temp = strtok(c_line," ,\t");
+		string line_tok[4];							//Define array of strings
+		char *temp = strtok(c_line," ,\t");					//Tokenize the string
 		line_tok[0] = temp;
-		short i=0;
+		int len=1;								//Counter for no. of tokens
 		while(temp!=NULL){
-			cout<<line_tok[i]<<"\t";
 			temp=strtok(NULL," ,\t");
 			if(temp==NULL)
 				break;
-			line_tok[++i] = temp;
+			line_tok[len++] = temp;						//Add token to Array of tokens
 		}
-		cout<<endl;
+		for(int i=0;i<len;i++)
+			cout<<line_tok[i]<<"\t";
+		cout<<"No. of Tokens in String is: "<<len<<endl;
+		/////////////////////////////////////////////////////////////////////////////////////////
 	}
 	reader.close();
 	return 0;
